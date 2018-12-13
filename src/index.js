705cee401b37fc86e1d14ed580d9ae8d77dbae13
Subhra Borkakati <subhra@mediasuite.co.nz>
2018-12-14 10:47:24 +1300

    Added AutoResizeInput to the exports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import PowerSelect from './PowerSelect';
 import TypeAhead from './TypeAhead';
 import TypeAheadSelectTrigger from './TypeAhead/SelectTrigger';
 import PowerSelectMultiple from './PowerSelectMultiple';
+import AutoResizeInput from './AutoResizeInput';
 
 TypeAhead.Trigger = TypeAheadSelectTrigger;
 
-export { PowerSelect, TypeAhead, PowerSelectMultiple };
+export { PowerSelect, TypeAhead, PowerSelectMultiple, AutoResizeInput };
